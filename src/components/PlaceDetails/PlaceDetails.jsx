8bce04db1d82e068cfccb2cb78f2ceff2b113d84
Jerry Chen <jhinresh@JerrydeMacBook-Pro.local>
2023-06-08 02:52:23 +0800

    placedetails

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -7,10 +7,8 @@ import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles.js';
 
 const PlaceDetails = ({ place, selected, refProp }) => {
+    if(selected) refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
     const classes = useStyles();
-
-    if(selected) refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start" })
-
 
     return (
         <Card elevation={6}>
@@ -21,21 +19,21 @@ const PlaceDetails = ({ place, selected, refProp }) => {
             />
             <CardContent>
                 <Typography gutterBottom variant='h5'>{place.name}</Typography>
-                <Box display="flex" justifyContent="space-between">
-                <Rating value={Number(place.rating)} readOnly />
-                    <Typography gutterBottom variant='subtitle1'>out of {place.num_reviews} reviews</Typography>
+                <Box display="flex" justifyContent="space-between" my={2}>
+                    <Rating name="read-only" value={Number(place.rating)} readOnly />
+                    <Typography component="legend">{place.num_reviews} review{place.num_reviews > 1 && 's'}</Typography>
                 </Box>
                 <Box display="flex" justifyContent="space-between">
-                    <Typography variant='subtitle1'>Price</Typography>
+                    <Typography component='legend'>Price</Typography>
                     <Typography gutterBottom variant='subtitle1'>{place.price_level}</Typography>
                 </Box>
                 <Box display="flex" justifyContent="space-between">
-                    <Typography variant='subtitle1'>Ranking</Typography>
+                    <Typography component="legend">Ranking</Typography>
                     <Typography gutterBottom variant='subtitle1'>{place.ranking}</Typography>
                 </Box>
                 {place?.awards?.map((award) => (
                     <Box key={award.display_name} my={1} display="flex" justifyContent="space-between" alignItems="center">
-                        <img src={award.images.small} alt={award.display_name} />
+                        <img src={award.images.small} />
                         <Typography variant='subtitle2' color='textSecondary'>{award.display_name}</Typography>
                     </Box>
                 ))}
@@ -43,15 +41,16 @@ const PlaceDetails = ({ place, selected, refProp }) => {
                     <Chip key={name} size='small' label={name} className={classes.chip} />
                 ))}
                 {place?.address && (
-                    <Typography gutterBottom variant='subtitle2' color="textSecondary" className={classes.subtitle}>
+                    <Typography gutterBottom variant='body2' color="textSecondary" className={classes.subtitle}>
                         <LocationOnIcon /> {place.address}
                     </Typography>
                 )}
                 {place?.phone && (
-                    <Typography gutterBottom variant='subtitle2' color="textSecondary" className={classes.spacing}>
+                     <Typography variant="body2" color="textSecondary" className={classes.spacing}>
                         <PhoneIcon /> {place.phone}
                     </Typography>
                 )}
+                </CardContent>
                 <CardActions>
                     <Button size='small' color='primary' onClick={() => window.open(place.web_url, '_blank')}>
                         SmartReviewHub
@@ -60,9 +59,8 @@ const PlaceDetails = ({ place, selected, refProp }) => {
                         Website
                     </Button>
                 </CardActions>
-            </CardContent>
         </Card>
     );
-}
+};
 
 export default PlaceDetails;
