2b5076ee4978403320432eb3d49dc2448d319cc6
Jerry Chen <jhinresh@JerrydeMacBook-Pro.local>
2023-06-08 03:11:02 +0800

    list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -9,9 +9,7 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
     const [elRefs, setElRefs] = useState([]);
 
     useEffect(() => {
-        const refs = Array(places?.length).fill().map((_, i) => elRefs[i] || createRef());
-
-        setElRefs(refs);
+        setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));
     }, [places]);
 
     return (
@@ -22,18 +20,18 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
                     <CircularProgress size='5rem' />
                 </div>
             ) : (
-                <>
+            <>
             <FormControl className={classes.formControl}>
-                <InputLabel>Type</InputLabel>
-                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                <InputLabel id='type'>Type</InputLabel>
+                <Select id='type' value={type} onChange={(e) => setType(e.target.value)}>
                     <MenuItem value="restaurants">Restaurants</MenuItem>
                     <MenuItem value="hotels">Hotels</MenuItem>
                     <MenuItem value="attractions">Attractions</MenuItem>
                 </Select>
             </FormControl>
             <FormControl className={classes.formControl}>
-                <InputLabel>Rating</InputLabel>
-                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                <InputLabel id='rating'>Rating</InputLabel>
+                <Select id='rating' value={rating} onChange={(e) => setRating(e.target.value)}>
                     <MenuItem value={0}>All</MenuItem>
                     <MenuItem value={3}>Above 3.0</MenuItem>
                     <MenuItem value={4}>Above 4.0</MenuItem>
@@ -42,7 +40,7 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
             </FormControl>
             <Grid container spacing={3} className={classes.list}>
                 {places?.map((place, i) => (
-                    <Grid ref={elRefs[i]} item key={i} xs={12}>
+                    <Grid ref={elRefs[i]} key={i} item xs={12}>
                         <PlaceDetails
                             place={place}
                             selected={Number(childClicked) === i}
@@ -55,6 +53,6 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
             )}
         </div>
     );
-}
+};
 
 export default List;
